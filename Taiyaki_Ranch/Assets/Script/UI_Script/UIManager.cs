58e503a46da0556a195729a8f7159f28b616faf1
Joyoung-jae <jaonz6057@gmail.com>
2022-01-13 04:10:36 +0900

    ++ 타이틀페이지 수정완료.

--- Taiyaki_Ranch/Assets/Script/UI_Script/UIManager.cs
+++ Taiyaki_Ranch/Assets/Script/UI_Script/UIManager.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject TitlePageImage;
+    public GameObject IngameHUD;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TitlePageImage.SetActive(true);
+        IngameHUD.SetActive(false);
+        Camera.main.GetComponent<Camera>().orthographicSize=3.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NewGame_OnClick()
+    {
+        TitlePageImage.SetActive(false);
+        Camera.main.GetComponent<Camera>().orthographicSize=5.4f;
+        IngameHUD.SetActive(true);
+    }
+
+    public void Continue_OnClick()
+    {
+        TitlePageImage.SetActive(false);
+        Camera.main.GetComponent<Camera>().orthographicSize=5.4f;
+        IngameHUD.SetActive(true);
+    }
+
+    public void Option_OnClick()
+    {
+
+    }
+
+    public void Exit_OnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying=false;
+#else
+        Application.Quit();
+#endif
     }
 
     public void Defend_OnMouseDown()
