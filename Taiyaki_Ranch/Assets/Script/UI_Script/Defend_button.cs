79bc7c915bed6f98ee730f998500e7a09fd17cdc
zcxzxc <zsz153@naver.com>
2022-01-12 21:44:08 +0900

    버튼 버그 수정 및 UI 텍스트 변경

--- Taiyaki_Ranch/Assets/Script/UI_Script/Defend_button.cs
+++ Taiyaki_Ranch/Assets/Script/UI_Script/Defend_button.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Defend_button : MonoBehaviour
 {
-    private void OnMouseDown()
+    public void button_down()
     {
         if (Enemy_data.Be_Battle == false)
         {
 
-             Enemy_data.Be_Battle = true;
-            for(int i=0;i<5;i++)
-             Camera.main.GetComponent<Enemy_data>().Rendering(1);
+            Enemy_data.Be_Battle = true;
+            for (int i = 0; i < 5; i++)
+                Camera.main.GetComponent<Enemy_data>().Rendering(1);
         }
-           
     }
 }
