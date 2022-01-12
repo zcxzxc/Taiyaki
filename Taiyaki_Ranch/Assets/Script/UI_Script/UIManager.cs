62a76e653d9d86bcc10c2eeb0a92526f953b9fad
Joyoung-jae <jaonz6057@gmail.com>
2022-01-12 21:19:11 +0900

    ++ Canvas UI ADD

---
+++ Taiyaki_Ranch/Assets/Script/UI_Script/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Defend_OnMouseDown()
+    {
+        if(Enemy_data.Be_Battle = false)
+        {
+            Enemy_data.Be_Battle = true;
+            for(int i=0; i<5; i++)
+                Camera.main.GetComponent<Enemy_data>().Rendering(1);
+        }
+    }
+}
