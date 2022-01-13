566fb7836852c2a2572a2c0c984b985fc48b8d62
Joyoung-jae <jaonz6057@gmail.com>
2022-01-13 18:48:26 +0900

    ++ IngameHUD.Buttons 추가(기능 미구현)

--- Taiyaki_Ranch/Assets/Script/Main_Camera/Load_Size_Up.cs
+++ Taiyaki_Ranch/Assets/Script/Main_Camera/Load_Size_Up.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Load_Size_Up : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.orthographicSize = 9f;
+        Camera.main.orthographicSize = 3.3f;
         StartCoroutine(Size_Up());
     }
     IEnumerator Size_Up()
     {
-        for(int i=0;i<6;i++)
+        for(int i=0;i<20;i++)
         {
-            Camera.main.orthographicSize -= 0.5f;
+            Camera.main.orthographicSize += 0.1f;
             yield return new WaitForSeconds(0.01f);
         }
         Camera.main.orthographicSize = 5.4f;
