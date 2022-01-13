e40935894445a4a1a8e487a8070a121fd12c6c5d
zcxzxc <zsz153@naver.com>
2022-01-13 17:44:08 +0900

    타이틀 씬 추가 및 UI코드 개선

---
+++ Taiyaki_Ranch/Assets/Script/Main_Camera/Load_Size_Up.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_Size_Up : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.orthographicSize = 9f;
+        StartCoroutine(Size_Up());
+    }
+    IEnumerator Size_Up()
+    {
+        for(int i=0;i<6;i++)
+        {
+            Camera.main.orthographicSize -= 0.5f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        Camera.main.orthographicSize = 5.4f;
+        Destroy(GetComponent<Load_Size_Up>());
+    }
+}
