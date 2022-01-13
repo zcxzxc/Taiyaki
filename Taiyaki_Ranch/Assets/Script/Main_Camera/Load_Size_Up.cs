ad021202b5670c3b192409fbe44ae4c091b644b9
Joyoung-jae <jaonz6057@gmail.com>
2022-01-14 00:21:59 +0900

    ++ 아이콘 추가 ++ [구름 애니메이션, 버튼 사라짐 애니메이션 추가 예정]

--- Taiyaki_Ranch/Assets/Script/Main_Camera/Load_Size_Up.cs
+++ Taiyaki_Ranch/Assets/Script/Main_Camera/Load_Size_Up.cs
@@ -5,21 +5,33 @@ using UnityEngine.UI;
 
 public class Load_Size_Up : MonoBehaviour
 {
+    public GameObject TitlePage_BackGround;
+    private Image TitlePage_BackGroundImage;
+
+    public GameObject InGameHUD_Top;
+    public GameObject InGaneHUD_Bottom;
+
+    Vector3 vectors;
 
     // Start is called before the first frame update
     void Start()
     {
+        TitlePage_BackGroundImage = TitlePage_BackGround.GetComponent<Image>();
         Camera.main.orthographicSize = 3.3f;
         StartCoroutine(Size_Up());
     }
     IEnumerator Size_Up()
     {
-        for(int i=0;i<20;i++)
+        float fadeCount = 0.5f;
+        for(int i=0;i<40;i++)
         {
-            Camera.main.orthographicSize += 0.1f;
+            fadeCount-=0.12f;
+            TitlePage_BackGroundImage.color = new Color(0,0,0, fadeCount);
+            Camera.main.orthographicSize += 0.05f;
             yield return new WaitForSeconds(0.01f);
         }
         Camera.main.orthographicSize = 5.4f;
+        TitlePage_BackGround.SetActive(false);
         Destroy(GetComponent<Load_Size_Up>());
     }
 }
